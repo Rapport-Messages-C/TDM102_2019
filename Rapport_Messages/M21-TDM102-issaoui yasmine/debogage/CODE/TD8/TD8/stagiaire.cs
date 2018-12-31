7105e22eabbd9feb461bd5ff3f25e71e198a6334
Stagiaire TP4 <tp.stagiaire.ismontic@gmail.com>
2018-12-31 10:44:30 +0100

    M21-TDM102-issaoui yasmine
    M21-TDM102-issaoui yasmine

---
+++ Rapport_Messages/M21-TDM102-issaoui yasmine/debogage/CODE/TD8/TD8/stagiaire.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD8
+{
+    class stagiaire
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string nom;
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        private string prenom;
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        private string cin;
+        public string Cin
+        {
+            get { return cin; }
+            set { cin = value; }
+        }
+        
+        public string presentation()
+        {
+            string presentation = "je suis " + this.Nom + " " + this.Prenom + " avec le CIN " + this.Cin;
+            return presentation;
+        }
+        public override string ToString()
+        {
+            return this.presentation();
+        }
+    }
+}
---
+++ Rapport_Messages/M21-TDM102-issaoui yasmine/exeption/CODE/exeption1/TD8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            stagiaire[] listeStagiaires = new stagiaire[2];
+
+            stagiaire stg1 = new stagiaire();
+            stg1.Id = 1;
+            stg1.Nom = "Mdani";
+            stg1.Prenom = "ali";
+            stg1.Cin = "K0001";
+            listeStagiaires[0] = stg1;
+
+            stagiaire stg2 = new stagiaire();
+            stg2.Id = 1;
+            stg2.Nom = "Mdani";
+            stg2.Prenom = "mouad";
+            stg2.Cin = "K0002";
+            listeStagiaires[1] = stg2;
+
+            foreach(stagiaire stg in listeStagiaires)
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine("stg : stagiaire " + stg);
+                Console.WriteLine("methode ToString : stagiaire " + stg.ToString());
+                Console.WriteLine("methode presentation : stagiaire " +stg.presentation());
+            
+            }
+            Console.Read();
+        }
+    }
+}
